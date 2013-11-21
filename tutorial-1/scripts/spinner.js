d1aaef36f8f78135aa82964aea799ab00c474d0e
Shaddock Heath <shaddockh@gmail.com>
2013-11-21 12:26:46 -0500

    initial spinner

---
+++ tutorial-1/scripts/spinner.js
@@ -0,0 +1,20 @@
+pc.script.create('spinner', function (context) {
+    // Creates a new Spinner instance
+    var Spinner = function (entity) {
+        this.entity = entity;
+    };
+
+    Spinner.prototype = {
+        // Called once after all resources are loaded and before the first update
+        initialize: function () {
+        },
+
+        // Called every frame, dt is time in seconds since last update
+        update: function (dt) {
+            //rotate 90 degrees
+            this.entity.rotate(0, 90*dt, 0);
+        }
+    };
+
+   return Spinner;
+});
