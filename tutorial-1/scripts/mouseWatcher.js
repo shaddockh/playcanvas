70de70ef8106cffae2fc3da55a8261e1fc02a5cc
Shaddock Heath <shaddockh@gmail.com>
2013-11-22 13:03:05 -0500

    Remove mouse watching from the watcher component

--- tutorial-1/scripts/mouseWatcher.js
+++ tutorial-1/scripts/mouseWatcher.js
@@ -1,41 +1,32 @@
 pc.script.create('mouseWatcher', function (context) {
 
+    
     //Component will watch the mouse and turn the entity to look at the mouse
-    var MouseHandler = function (entity) {
+    var WatchTarget = function (entity) {
         this.entity = entity;
         this._watchTarget = 'watchme';
-        this.targetPosition = pc.math.vec3.create();
-
-
-        // Use the on() method to attach event handlers.
-        // The mouse object supports events on move, button down and
-        // up, and scroll wheel.
-        context.mouse.on(pc.input.EVENT_MOUSEMOVE, this.onMouseMove, this);
+        this._wathced = null;
     };
 
-    MouseHandler.prototype = {
-        onMouseMove: function (event) {
-            
-            //find the mouseLight and then point at it.
-            var target = null;
-            var target = context.root.findByLabel(this._watchTarget);
-            //console.log(target);
-            if (target && target.length > 0) {
-                this.entity.lookAt(target[0].localPosition);
+    WatchTarget.prototype = {
+        
+        getWatched: function() {
+            if (!(this._watched)) {
+                var target = context.root.findByLabel(this._watchTarget);
+                if (target && target.length > 0) {
+                    this._watched = target[0];
+                }
             }
-            
-            // Get the current camera Entity
-            //var cameraEntity = context.systems.camera.current
-
-            // Use the camera component's screenToWorld function to convert the
-            // position of the mouse into a position in 3D space
-            //var depth = 10;
-            //cameraEntity.camera.screenToWorld(event.x, event.y, depth, this.targetPosition);
-
-            //TODO: move the tracking of the mouse in world space to a single entity at the root
-            //this.entity.lookAt(this.targetPosition,  pc.math.vec3.create(0, -1, 0));
+            return this._watched;
+        },
+        update: function(delta) {
+            //find the mouseLight and then point at it.
+            var target = this.getWatched();
+            if (target) {
+                this.entity.lookAt(target.localPosition);
+            }
         }
     };
 
-    return MouseHandler;
+    return WatchTarget;
 });
