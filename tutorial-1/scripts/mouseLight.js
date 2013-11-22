6891735a32198a048a711fceb3cf1aff2c335107
Shaddock Heath <shaddockh@gmail.com>
2013-11-21 21:45:54 -0600

    cleaning up the components

--- tutorial-1/scripts/mouseLight.js
+++ tutorial-1/scripts/mouseLight.js
@@ -1,12 +1,10 @@
 /* global: pc */
 pc.script.create('mouseLight', function (context) {
 
-
+    //Component will move the entity to follow the mouse
     var MouseHandler = function (entity) {
         this.entity = entity;
         this.pos = pc.math.vec3.create();
-
-        this.targetPosition = pc.math.vec3.create();
 
         // Disabling the context menu stops the browser displaying a menu when
         // you right-click the page
@@ -16,7 +14,6 @@ pc.script.create('mouseLight', function (context) {
         // The mouse object supports events on move, button down and
         // up, and scroll wheel.
         context.mouse.on(pc.input.EVENT_MOUSEMOVE, this.onMouseMove, this);
-        context.mouse.on(pc.input.EVENT_MOUSEDOWN, this.onMouseDown, this);
     };
 
     MouseHandler.prototype = {
@@ -27,34 +24,14 @@ pc.script.create('mouseLight', function (context) {
             // Use the camera component's screenToWorld function to convert the
             // position of the mouse into a position in 3D space
             var depth = 10;
-            cameraEntity.camera.screenToWorld(event.x, event.y, depth, this.targetPosition);
+            cameraEntity.camera.screenToWorld(event.x, event.y, depth, this.pos);
 
             // Finally update the cube's world-space position
             //console.log(this.pos);
 
-            this.entity.setPosition(this.targetPosition);
-        },
-
-        xxonMouseDown: function (event) {
-            // If the left mouse button is pressed, change the cube color to red
-            if (event.button === pc.input.MOUSEBUTTON_LEFT) {
-                this.entity.primitive.color = new pc.Color(1,0,0);
-            }
-
-            // If the left mouse button is pressed, change the cube color to green
-            if (event.button === pc.input.MOUSEBUTTON_MIDDLE) {
-                this.entity.primitive.color = new pc.Color(0,1,0);
-            }
-
-            // If the left mouse button is pressed, change the cube color to blue
-            if (event.button === pc.input.MOUSEBUTTON_RIGHT) {
-                this.entity.primitive.color  = new pc.Color(0,0,1);
-            }
+            this.entity.setPosition(this.pos);
         }
     };
 
-
     return MouseHandler;
-
-
 });
--- tutorial-1/scripts/mouseWatcher.js
+++ tutorial-1/scripts/mouseWatcher.js
@@ -1,26 +1,15 @@
-/* global: pc */
 pc.script.create('mouseWatcher', function (context) {
 
-
+    //Component will watch the mouse and turn the entity to look at the mouse
     var MouseHandler = function (entity) {
         this.entity = entity;
-        this.pos = pc.math.vec3.create();
-
         this.targetPosition = pc.math.vec3.create();
 
-
-        //this.entity.primitive.material = new pc.scene.BasicMaterial();
-        //this.entity.primitive.material.color.set(43,43,43,100);
-
-        // Disabling the context menu stops the browser displaying a menu when
-        // you right-click the page
-        context.mouse.disableContextMenu();
 
         // Use the on() method to attach event handlers.
         // The mouse object supports events on move, button down and
         // up, and scroll wheel.
         context.mouse.on(pc.input.EVENT_MOUSEMOVE, this.onMouseMove, this);
-        context.mouse.on(pc.input.EVENT_MOUSEDOWN, this.onMouseDown, this);
     };
 
     MouseHandler.prototype = {
@@ -33,32 +22,10 @@ pc.script.create('mouseWatcher', function (context) {
             var depth = 10;
             cameraEntity.camera.screenToWorld(event.x, event.y, depth, this.targetPosition);
 
-            // Finally update the cube's world-space position
-            //console.log(this.pos);
-
+            //TODO: move the tracking of the mouse in world space to a single entity at the root
             this.entity.lookAt(this.targetPosition,  pc.math.vec3.create(0, -1, 0));
-        },
-
-        xxonMouseDown: function (event) {
-            // If the left mouse button is pressed, change the cube color to red
-            if (event.button === pc.input.MOUSEBUTTON_LEFT) {
-                this.entity.primitive.color = new pc.Color(1,0,0);
-            }
-
-            // If the left mouse button is pressed, change the cube color to green
-            if (event.button === pc.input.MOUSEBUTTON_MIDDLE) {
-                this.entity.primitive.color = new pc.Color(0,1,0);
-            }
-
-            // If the left mouse button is pressed, change the cube color to blue
-            if (event.button === pc.input.MOUSEBUTTON_RIGHT) {
-                this.entity.primitive.color  = new pc.Color(0,0,1);
-            }
         }
     };
 
-
     return MouseHandler;
-
-
 });
