e1fe3e90ad6937ef93983f0964bdfde482e4e041
Shaddock Heath <shaddockh@gmail.com>
2013-12-05 06:16:48 -0600

    adding a component that will hide an entity and added a target component for the barrels

---
+++ isoshooter/statictarget.js
@@ -0,0 +1,49 @@
+/* global: pc */
+
+/**
+ * @module isoshooter
+ * @namespace isoshooter
+ */
+pc.script.create('statictarget', function (context) {
+
+    /**
+     * Description
+     *
+     * @class statictarget
+     * @param entity
+     * @constructor
+     */
+    var statictargetComponent = function (entity) {
+        this.entity = entity;
+    };
+
+    statictargetComponent.prototype = {
+        /**
+         * Called once after all resources are loaded and before the first update
+         * @method initialize
+         */
+        initialize: function () {
+            this.entity.collision.on('contact', this.onContact, this);
+        },
+
+
+        /**
+         * @method onContact
+         */
+        onContact: function(result) {
+            console.log('hit by', result);
+            this.entity.destroy();
+        },
+
+        /**
+         * Called every frame
+         * @method update
+         * @param {number} delta The amount of time in seconds since last update
+         */
+        update: function (delta) {
+
+        }
+    };
+
+    return statictargetComponent;
+});
