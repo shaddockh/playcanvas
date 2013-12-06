133e92ee93484a47b6574f44dd0a066cbaccecc8
Shaddock Heath <shaddockh@gmail.com>
2013-12-06 07:39:06 -0600

    adding linting and beautifying to the grunt process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,52 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        yuidoc: {
-            compile: {
-                name: '<%= pkg.name %>',
-                description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
-                url: '<%= pkg.homepage %>',
-                options: {
-                    paths:['tutorial-1/',
-                        'lander_ex/',
-                        'isoshooter/'
-                    ],
-                    //themedir: 'path/to/custom/theme/',
-                    outdir: 'docs'
-                }
-            }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: ['tutorial-1/',
+            'lander_ex/',
+            'isoshooter/'
+          ],
+          //themedir: 'path/to/custom/theme/',
+          outdir: 'docs'
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', '_fe/js/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    jsbeautifier: {
+      modify: {
+        src: ['Gruntfile.js', '_fe/js/**/*.js'],
+        options: {
+          config: '.jsbeautifyrc'
+        }
+      },
+      verify: {
+        src: ['Gruntfile.js', '_fe/js/**/*.js'],
+        options: {
+          mode: 'VERIFY_ONLY',
+          config: '.jsbeautifyrc'
+        }
+      }
+    }
 
-    grunt.registerTask('docs', ['yuidoc']);
+  });
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+
+  grunt.registerTask('docs', ['yuidoc']);
+
+  grunt.registerTask('clean', ['jsbeautifier:modify', 'jshint']);
+  grunt.registerTask('verify', ['jsbeautifier:verify', 'jshint']);
+  grunt.registerTask('default', ['verify', 'docs']);
 };
